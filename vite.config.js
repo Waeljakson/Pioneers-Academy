1b95935f8cfeb469886ed8c8ac995467f10b8d54
Waeljakson <waeljaksonstar@gmail.com>
2026-09-25 19:34:43 +0300

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  base: "/Pioneers-Academy/",
   plugins: [react()],
   server: { proxy: { "/api": "http://127.0.0.1:3000" } },
   build: { sourcemap: false },
